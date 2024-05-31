1dd62a42de78c6d41d7f24f1ff08fab442a8ba99
Iyanuoluwa Ogunfiditimi <iyanu.mehlek22@gmail.com>
2024-05-31 00:12:03 -0400

    First Commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="Main">
+    <div class="Title"><h1 id="satisfy-regular">HUNCHO</h1></div>
+    
+    <ul>
+      <li><a href="">Merch</a></li>
+      <li><a href="">About Me</a></li>
+    </ul>
+    
+    <SongViewer :imgPath="require('@/assets/Shooting Stars/Shooting Stars Cover.png')" class="SongImage"/>
+          
+    <div><p> <a href="">Subscribe</a> to Huncho's newsletter</p></div>
+    
+  </div>
+</template>
+
+<script>
+import SongViewer from './SongViewer.vue';
+
+export default {
+  name: 'MainPage',
+  components: {
+    SongViewer
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Satisfy&display=swap');
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.Title {
+  background-color: brown;
+  padding: 1px;
+}
+
+.Main {
+  background-color: black;
+  text-align: center;
+  color: white;
+}
+
+#satisfy-regular {
+  font-family: "Satisfy", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
---
+++ src/components/SongViewer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <img :src="imgPath" alt="Image Here" class="ImageSize">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SongViewer',
+    props: {
+      imgPath: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+
+  <style scoped>
+    .ImageSize {
+        max-height: 200px;
+        width: auto;
+    }
+    
+    </style>
